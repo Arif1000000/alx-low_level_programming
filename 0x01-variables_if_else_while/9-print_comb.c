0579e06ebcb3036e49cc3cde43bca7881ea1d768
issah arif <muhammed.arif11@gmail.com>
2022-12-11 18:33:50 -0800

    Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all combination of single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num >= '0' && num <= '9')
+		{
+			putchar(num);
+		}
+		if (num >= '0' && num < '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
