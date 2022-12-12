eb72b092045b7d540b2314a3b2c6c5d55612bbeb
issah arif <muhammed.arif11@gmail.com>
2022-12-12 13:20:18 -0800

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main - prints all possibilities of two digits
+ * Return: 0 if (succesful)
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i = 0; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if ((i == 98) && (j == 99))
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
