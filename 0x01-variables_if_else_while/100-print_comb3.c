3c9349c4b08144fb9f7d5bd1a8ba21d63c483dff
issah arif <muhammed.arif11@gmail.com>
2022-12-12 12:02:15 -0800

    Print combinatio3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - takes unspecified number of arguments and returns value
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	int i, j;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
-
+	i = 48;
+	j = 49;
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(j);
+		if ((i == 56) && (j == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (j < 57)
+		{
+			j++;
+		}
+		else
+		{
+			i++;
+			j = i + 1;
+		}
+	}
 	return (0);
 }
