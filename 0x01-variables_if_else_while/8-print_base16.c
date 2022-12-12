e715ef5c70d5cd4619762e6cb98818f1fff58bfe
issah arif <muhammed.arif11@gmail.com>
2022-12-11 18:04:46 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int al, an;
+
+
+	for (al = '0'; al <= '9'; al++)
+		putchar(al);
+	for (an = 97; a <=102' an++)
+		putchar(an);
+	putchar('\n');
+
+	return (0);
+}
