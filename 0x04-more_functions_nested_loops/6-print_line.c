ac648adcdf6f0269c5ee489fb5945f0c8eb056e0
issah arif <muhammed.arif11@gmail.com>
2022-12-16 13:34:37 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line n times.
+ *
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_lie(int n)
+{
+	int i;
+
+	for (i = 0; i < ; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
