ea43181c27a774fc1fd274825c47cfb1d6818dff
issah arif <muhammed.arif11@gmail.com>
2022-12-19 09:19:26 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/putchar.c
@@ -0,0 +1,11 @@
+#include <unistdio.h>
+
+/**
+ * _putchar - writed character c to stdout
+ * @c: char to be printed
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
